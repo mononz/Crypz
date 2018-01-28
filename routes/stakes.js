9f622b2557024fb6dd4931c3c8b634d7751cc477
Jared Hall <jaredrexhall@gmail.com>
2018-01-29 01:00:58 +1100

    temporarily setting prices to 1000

--- routes/stakes.js
+++ routes/stakes.js
@@ -1,9 +1,12 @@
 const Router = require('koa-router');
 const router = new Router({ prefix: '/stakes' });
 
+const timeout = ms => new Promise(res => setTimeout(res, ms));
+
 router.post('/', async ctx => {
+  //await timeout(2000);
   ctx.request.body.forEach((item) => {
-    item['price'] = 15
+    item['price'] = 1000
   });
   ctx.body = ctx.request.body;
 });
