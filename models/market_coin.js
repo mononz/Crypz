fffd9be77b65ae391e0130a722b3746ed950e062
Jared Hall <jaredrexhall@gmail.com>
2018-01-31 00:11:34 +1100

    api look ups for /stakes. Closes #6

--- models/market_coin.js
+++ models/market_coin.js
@@ -7,6 +7,13 @@ let model = bookshelf.Model.extend({
 }, {
   sync: function(updatedAt) {
     return model.forge().where('updated_at', '>', updatedAt).fetchAll();
+  },
+  details: function (marketCoinId) {
+    return bookshelf.knex('market_coin')
+      .innerJoin('market', 'market.market_id', 'market_coin.market_id')
+      .innerJoin('coin', 'coin.coin_id', 'market_coin.coin_id')
+      .where('market_coin_id', marketCoinId)
+      .first();
   }
 });
 
