02e95a8b75d142e82b29a3997a5bb718710504f5
Jared Hall <jaredrexhall@gmail.com>
2018-05-14 23:42:30 +1000

    knexfile and env template updated

---
+++ knexfile.js
@@ -0,0 +1,60 @@
+// Update with your config settings.
+var dotenv = require('dotenv');
+dotenv.load();
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      host:     'localhost',
+      database: 'planetside',
+      user:     'planetside',
+      password: 'planetside',
+      port:     '5432'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  staging: {
+    client: process.env.DB_CLIENT,
+    connection: {
+      host:     process.env.DB_HOST,
+      database: process.env.DB_DATABASE,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASS,
+      port:     process.env.DB_PORT
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: process.env.DB_CLIENT,
+    connection: {
+      host:     process.env.DB_HOST,
+      database: process.env.DB_DATABASE,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASS,
+      port:     process.env.DB_PORT
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+};
